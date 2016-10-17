d951d1c2e8c7ba8a30fa476d7979e39256c724bb
Yuchen Shi <yuchenshi@cmu.edu>
2016-10-17 15:53:34 -0400

    Add login modal and navbar state.

--- public/javascript/succotash.js
+++ public/javascript/succotash.js
@@ -1,6 +1,41 @@
 (function() {
   var app = angular.module('succotash', ['ui.router']);
 
-  app.controller('MasterCtrl', function() {
+  app.controller('MasterCtrl', function($state) {
+    var master = this;
+    master.$state = $state;
+    master.showLoginModal = showLoginModal;
+    master.login = login;
+    master.logout = logout;
+
+    init();
+
+    function showLoginModal() {
+      master.loginDetails = {};
+      master.loginModalShown = true;
+    }
+
+    function login(loginDetails) {
+      master.user = loginDetails;
+      localStorage['upmcSession'] = JSON.stringify(master.user);
+      master.loginModalShown = false;
+    }
+
+    function logout() {
+      master.user = null;
+      delete localStorage['upmcSession'];
+      master.loginModalShown = false;
+      master.showUserMenu = false;
+    }
+
+    function init() {
+      var loginDetails = null;
+      try {
+        loginDetails = JSON.parse(localStorage['upmcSession'] || 'null');
+      } catch (_) {}
+      if (loginDetails) {
+        login(loginDetails);
+      }
+    }
   });
 })();
